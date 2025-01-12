47d1f26076c58680bb75225f51fb58f3a76b39c6
Etxaaniiz <aimar.etxaniz@opendeusto.es>
2025-01-12 18:43:07 +0100

    Borrado de carpetas generadas

--- user-service/app/__init__.py
+++ user-service/app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from flask_migrate import Migrate  # ✅ Importar Migrate
+from flask_migrate import Migrate  
 
 import logging
 
